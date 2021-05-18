25d0cf922798d1d1e1796c1bc70bcd3218b9ebd8
Greg Doud <gpdoud@gmail.com>
2021-05-17 21:52:30 -0400

    Fixed address components

--- src/app/feature/address/address.ts
+++ src/app/feature/address/address.ts
@@ -12,28 +12,28 @@ export class Address {
   dateCreated: string;
 
   constructor(
-    Id: number,
-    Code: string,
-    Name: string,
-    Address1: string,
-    Address2: string,
-    Address3: string,
-    City: string,
-    State: string,
-    ZipCode: string,
-    Active: boolean = true,
-    DateCreated: string = new Date().toISOString()
+    id: number,
+    code: string,
+    name: string,
+    address1: string,
+    address2: string,
+    address3: string,
+    city: string,
+    state: string,
+    zipCode: string,
+    active: boolean = true,
+    dateCreated: string = new Date().toISOString()
   ) {
-    this.id = Id;
-    this.code = Code;
-    this.name = Name;
-    this.address1 = Address1;
-    this.address2 = Address2;
-    this.address3 = Address3;
-    this.city = City;
-    this.state = State;
-    this.zipCode = ZipCode;
-    this.active = Active;
-    this.dateCreated = DateCreated;
+    this.id = id;
+    this.code = code;
+    this.name = name;
+    this.address1 = address1;
+    this.address2 = address2;
+    this.address3 = address3;
+    this.city = city;
+    this.state = state;
+    this.zipCode = zipCode;
+    this.active = active;
+    this.dateCreated = dateCreated;
   }
 }
